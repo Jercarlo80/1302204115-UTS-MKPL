6fcb6eccb7e61b6e7b2bc792abb4ff8b703aefb2
jercarlo30 <jloprodaction80@gmail.com>
2023-04-14 19:52:26 +0700

    create-tax-single-tax-married-tax-child

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -1,10 +1,13 @@
 package lib;
 
 public class TaxFunction {
-	
+	private static final int tax_single = 54000000;
+	private static final int tax_married = 58500000;
+	private static final int tax_child = 1500000;
 	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
 		
 		int tax = 0;
+		
 		
 		if (numberOfMonthWorking > 12) {
 			System.err.println("More than 12 month working per year");
@@ -15,11 +18,13 @@ public class TaxFunction {
 		}
 		
 		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
+			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (tax_single + 4500000 + (numberOfChildren * tax_child))));
 		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
+			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - tax_single));
 		}
 		
+
+
 		if (tax < 0) {
 			return 0;
 		}else {
